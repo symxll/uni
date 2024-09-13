61be7460ed096a2aa36600900d31d82ea19c596e
Li Heng <133302251+symxll@users.noreply.github.com>
2024-09-13 15:50:53 +0800

    Add files via upload

---
+++ pages/login/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <view>
+    <up-navbar title="登录"></up-navbar>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style>
+
+</style>
